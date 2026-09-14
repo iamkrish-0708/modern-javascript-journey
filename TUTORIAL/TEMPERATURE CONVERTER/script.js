9e63d1add51b77b8a886378deb8a0d3e31ef001f
Krish <krishj072008@gmail.com>
2026-09-14 17:53:58 +0530

    feat(tutorial): build interactive Temperature Converter program with unit conversion formulas and .toFixed() formatting

---
+++ TUTORIAL/TEMPERATURE CONVERTER/script.js
@@ -0,0 +1,21 @@
+const input=document.getElementById("TEMPIO");
+const toFah=document.getElementById("toFahrenheit");
+const toCel=document.getElementById("toCelsius");
+const calc=document.getElementById("SUBMIT");
+const output=document.getElementById("RESULT");
+
+calc.onclick=function(){
+    let temperature=Number(input.value);
+    if(toFah.checked){
+        temperature=(temperature*(9/5))+32;
+        output.textContent=`${Number(input.value)} (in °C) = ${temperature.toFixed(1)} (in  °F)`;
+    }
+    else if(toCel.checked){
+        temperature=(temperature-32)*(5/9);
+        output.textContent=`${Number(input.value)} (in °F) = ${temperature.toFixed(1)} (in °C)`;
+    }
+    else{
+        output.textContent="SELECT AT LEAST ONE OPTION OF CONVERSION.";
+    }
+
+}
